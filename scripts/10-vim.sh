ebdcd26feabba69677614e1212d8eb0c6ea4c5ca
Martin Höger <martin.hoger@gmail.com>
2017-08-10 10:57:33 +0200

    Update 10-vim.sh

--- scripts/10-vim.sh
+++ scripts/10-vim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install git wget vim-nox
+sudo apt-get install -y git wget vim-nox
 curl -L https://raw.githubusercontent.com/martin-hoger/.vim/master/bin/install.sh | sh
 #Set default editor
 update-alternatives --set editor /usr/bin/vim.nox
