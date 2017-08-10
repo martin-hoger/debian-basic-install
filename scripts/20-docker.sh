a36099bc2a51b6a9037fc46a19713c54fd5344f1
Martin Höger <martin.hoger@gmail.com>
2017-08-10 11:04:00 +0200

    Update 20-docker.sh

--- scripts/20-docker.sh
+++ scripts/20-docker.sh
@@ -7,4 +7,4 @@ echo 'deb https://apt.dockerproject.org/repo debian-stretch main' | sudo tee -a 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-get update
 
-sudo apt-get install -y docker-engine
+sudo apt-get install -y --allow-unauthenticated docker-engine
