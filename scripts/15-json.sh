977d6587183b16c08dce81e6f041fc3456361a68
default-debian-install <root@default-debian-install>
2017-08-10 08:16:57 +0000

    JSON install script changed

--- scripts/15-json.sh
+++ scripts/15-json.sh
@@ -2,5 +2,5 @@
 sudo apt-get install -y nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
-sudo curl -L https://github.com/trentm/json/raw/master/lib/json.js > /usr/bin/json
+curl -L https://github.com/trentm/json/raw/master/lib/json.js | sudo tee /usr/bin/json > /dev/null
 
