a35e03f29424c8af5ec77549d831d22acd9682ad
default-debian-install <root@default-debian-install>
2017-08-10 08:24:30 +0000

    Global parameter added

--- scripts/06-git.sh
+++ scripts/06-git.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git config credential.helper store
+git config --global credential.helper store
 git config --global user.email "root@$(hostname)"
 git config --global user.name $(hostname)
