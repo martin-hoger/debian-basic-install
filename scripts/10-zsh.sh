f8f33d0666e7f4b9310d0c56aa5d9d618ea489be
Martin Höger <martin.hoger@gmail.com>
2017-08-10 11:01:40 +0200

    Update 10-zsh.sh

--- scripts/10-zsh.sh
+++ scripts/10-zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install zsh
+sudo apt-get install -y zsh
 sudo chsh -s $(which zsh) $(whoami)
 curl -L https://raw.githubusercontent.com/martin-hoger/.zsh/master/bin/install.sh | sh
 
